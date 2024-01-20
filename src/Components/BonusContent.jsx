1c6aa5a60e8853901f67b58089b5898beaa35c60
Onur Esin <onuresn034@gmail.com>
2024-01-20 20:15:39 +0300

    bonus oyun modu web kısmı css ile stillendirildi

--- src/Components/BonusContent.jsx
+++ src/Components/BonusContent.jsx
@@ -6,6 +6,12 @@ import Paper from "../assets/paper-pol.svg"
 import Lizzard from "../assets/lizzard.svg"
 import Rock from "../assets/rock-pol.svg"
 import Close from "../assets/close-shape.svg";
+import PolygonRes from "../assets/Polygon-res.svg"
+import ScissorsRes from "../assets/scissors-res.svg"
+import SpockRes from "../assets/spock-res.svg"
+import PaperRes from "../assets/paper-res.svg"
+import RockRes from "../assets/rock-res.svg"
+import LizzardRes from "../assets/Lizzard-res.svg"
 
 
 
@@ -54,6 +60,20 @@ export default function BonusContent() {
                             <button><img src={Rock} alt="rock"/></button>
                         </div>
                     </div>
+                    <div className="bonusContent-gamebtnsRes">
+                    <img className="polygonRes" src={PolygonRes}/>
+                        <div className="bonusGamebtns-topRes">
+                            <button><img src={ScissorsRes} alt="scissorsRes" /></button>
+                        </div>
+                        <div className="bonusGamebtns-midRes">
+                            <button><img src={SpockRes} alt="spockRes"/></button>
+                            <button><img src={PaperRes} alt="paperRes"/></button>
+                        </div>
+                        <div className="bonusGamebtns-botRes">
+                            <button><img src={LizzardRes} alt="lizzardRes"/></button>
+                            <button><img src={RockRes} alt="rockRes"/></button>
+                        </div>
+                    </div>
                     <div className="footer">
                         <div className="modal-container">
                         <button className='modal-btn' onClick={toggleModal}>KURALLAR</button>
@@ -62,7 +82,10 @@ export default function BonusContent() {
                             <div className='modal-section'>
                             <div onClick={toggleModal} className="before-opened"></div>
                             <div className="modal-inner">
+                            <div className="top-sec">
                                 <h4>KURALLAR</h4>
+                                <button className='modal-closeRes' onClick={toggleModal}><img src={Close} alt="Close" /></button>
+                            </div>
                                 <div className="modal-content-polygon">
                                     <span className='top-polygon'>
                                         <h3>YENER</h3>
